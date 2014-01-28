aef48c3e38de5ba82ef4b8aab86835267585e1fe
Patrick Stankard <me@patrickstankard.com>
2014-01-28 18:07:31 -0500

    dump

---
+++ js/teambeers.js
@@ -0,0 +1,77 @@
+(function(w) {
+  'use strict';
+
+  var TeamBeers = function() {
+    var self = this;
+
+    self.answer = false;
+
+    $(function() {
+      self.$answer = $('#teambeers-answer');
+
+      self.calculateAndUpdateAnswer();
+
+      self.calculateAndUpdateAnswerInterval = setInterval(function() {
+        self.calculateAndUpdateAnswer();
+      }, 1000);
+    });
+  };
+
+  TeamBeers.prototype.calculateAnswer = function() {
+    var now, day, hours, minutes;
+
+    now = new Date();
+
+    day = {
+      value: now.getUTCDay()
+    };
+
+    hours = {
+      value: now.getUTCHours()
+    };
+
+    minutes = {
+      value: now.getUTCMinutes()
+    };
+
+    day.pass = day.value === 5;
+    hours.pass = hours.value >= 21;
+    minutes.pass = minutes.value >= 45 && hours.pass === true || hours.value >= 22;
+
+    if (day.pass === true && hours.pass === true && minutes.pass === true) {
+      this.answer = true;
+    }
+  };
+
+  TeamBeers.prototype.updateAnswer = function() {
+    var text = {
+      current: this.$answer.text(),
+      latest: this.answer === true ? 'YES' : 'NO'
+    };
+
+    if (text.current !== text.latest) {
+      this.$answer.text(text.latest);
+    }
+  };
+
+  TeamBeers.prototype.calculateAndUpdateAnswer = function() {
+    this.calculateAnswer();
+    this.updateAnswer();
+  };
+
+  TeamBeers.prototype.manualOverride = function(params) {
+    var self = this;
+
+    if (typeof params === 'object') {
+      if (typeof params.answer === 'boolean') {
+        self.answer = params.answer;
+
+        clearInterval(self.calculateAndUpdateAnswerInterval);
+        self.updateAnswer();
+      }
+    }
+  };
+
+  w.teamBeers = new TeamBeers();
+
+})(window);
